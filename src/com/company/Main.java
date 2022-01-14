ad189e2fba7ea71c8a3a339fa35ec9d63acff11c
Haris Avdicevic <71407043+chubrubbaloo@users.noreply.github.com>
2022-01-14 16:17:57 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
 
         /*
-        MyFunc doubleMe = new MyFunc() {
+        MyFunc squared = new MyFunc() {
             @Override
             public int apply(int x) {
                 return x * x;
